a6c3386ebe9b3831f32e4166658f2b6ce649c9a4
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-08 09:16:10 -1000

    Move the block comment to the bottom of the file and compress it.
    This'll make coding the stuff easier.

--- op.rb
+++ op.rb
@@ -3,27 +3,19 @@
 require 'matrix'
 
 =begin
+
 base matrix equation: [A][OPR] = [SCORE]
-
 define [A]^t to be [A] transpose
-
 define [P] to be [A][A]^t
-
 define [S] to be [SCORE][A]^t
-
 equation is now [P][OPR] = [S]
-
 refactor [P] as [L][L]^t using cholesky
-
 Therefore [L][L]^t[OPR] = [S]
-
 define [Y] = [L]^t[OPR]
-
 equation is now [L][Y] = [S]
-
 find [Y] through forward substitution
-
 find [OPR] through backward substitution
+
 =end
 
 # props to http://rosettacode.org/wiki/Cholesky_decomposition#Ruby :L
@@ -88,3 +80,19 @@ lt.output
 
 puts "Original (from teh other stuffies)"
 (l * lt).output
+
+=begin
+
+base matrix equation: [A][OPR] = [SCORE]
+define [A]^t to be [A] transpose
+define [P] to be [A][A]^t
+define [S] to be [SCORE][A]^t
+equation is now [P][OPR] = [S]
+refactor [P] as [L][L]^t using cholesky
+Therefore [L][L]^t[OPR] = [S]
+define [Y] = [L]^t[OPR]
+equation is now [L][Y] = [S]
+find [Y] through forward substitution
+find [OPR] through backward substitution
+
+=end
