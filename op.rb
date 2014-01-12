e117f298816362256fe5b9f3e7740cfd4f9eca51
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-01-11 22:16:20 -0600

    Matrix multiplication does not have the commutative property.

--- op.rb
+++ op.rb
@@ -68,10 +68,11 @@ end
 =begin
 base matrix equation: [A][OPR] = [SCORE]
 define [A]^t to be [A] transpose
-define [P] to be [A][A]^t
-define [S] to be [SCORE][A]^t
+define [P] to be [A]^t[A]
+define [S] to be [A]^t[SCORE]
 equation is now [P][OPR] = [S]
 refactor [P] as [L][L]^t using cholesky
+[L] is a lower triangular matrix
 Therefore [L][L]^t[OPR] = [S]
 define [Y] = [L]^t[OPR]
 equation is now [L][Y] = [S]
