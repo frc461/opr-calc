b55922cf91c9b3ac9b3e11df93b3a3b353d4f960
Sammidysam <sammidysam@gmail.com>
2014-01-25 13:26:15 -0500

    Some prettification of license stub and single quotes

--- lib/opr-calc.rb
+++ lib/opr-calc.rb
@@ -1,22 +1,22 @@
 =begin
-    opr-calc is a tool for calculating OPR and other scouting stats for FRC teams.
-    Copyright (C) 2014 Kristofer Rye and Christopher Cooper
+	opr-calc is a tool for calculating OPR and other scouting stats for FRC teams.
+	Copyright (C) 2014 Kristofer Rye and Christopher Cooper
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
-require 'matrix'
+require "matrix"
 
 # Props to http://rosettacode.org/wiki/Cholesky_decomposition#Ruby :L
 class Matrix
