b5c47bdb5c135aeee98da91a43426d99b0c014d2
Sammidysam <sammidysam@gmail.com>
2014-01-25 14:16:50 -0500

    Add license stubs

--- test/test_matrix.rb
+++ test/test_matrix.rb
@@ -1,3 +1,21 @@
+=begin
+	opr-calc is a tool for calculating OPR and other scouting stats for FRC teams.
+	Copyright (C) 2014 Kristofer Rye and Christopher Cooper
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 # Here is where the added functions to the Matrix class are tested.
 
 require "minitest/autorun"
